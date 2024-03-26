fdb89af6556389c40bb897aac7c2f87590c4b97d
Spiros Ioannou <sioannou@grnet.gr>
2024-03-26 23:39:59 +0200

    Fix Configuration for Images

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "https://digitalhippo-production-a297.up.railway.app/",
-			},
+		domains: [
+				'localhost',
+				'digitalhippo-production-a297.up.railway.app',
 		],
 	},
 };
